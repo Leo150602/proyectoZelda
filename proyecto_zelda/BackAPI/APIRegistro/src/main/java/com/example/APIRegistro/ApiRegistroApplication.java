e5b2c2607edaa0ac3bb52fdf24a742538dcc7110
EstudianteCesde <EstudianteCesde@gmail.com>
2025-07-24 11:28:42 -0500

    avance1

--- proyecto_zelda/BackAPI/APIRegistro/src/main/java/com/example/APIRegistro/ApiRegistroApplication.java
+++ proyecto_zelda/BackAPI/APIRegistro/src/main/java/com/example/APIRegistro/ApiRegistroApplication.java
@@ -2,6 +2,9 @@ package com.example.APIRegistro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ApiRegistroApplication {
@@ -10,4 +13,17 @@ public class ApiRegistroApplication {
 		SpringApplication.run(ApiRegistroApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**") // permite CORS en todos los endpoints
+						.allowedOrigins("*") // permite cualquier origen, puedes poner "http://localhost:3000" si quieres restringir
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*");
+			}
+		};
+	}
 }
+
